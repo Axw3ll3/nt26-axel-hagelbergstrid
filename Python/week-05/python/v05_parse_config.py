3ad5e5f4f929b299512056ef51194f96351e89d6
Axw3ll3 <axel.hagelbergstridh@gmail.com>
2026-09-15 16:17:48 +0200

    Did python exercise, added file and a running config aswell as included interfaces

--- Python/week-05/python/v05_parse_config.py
+++ Python/week-05/python/v05_parse_config.py
@@ -1,13 +1,20 @@
-filnamn = "r1-show-run.txt"
+filnamn = "configs/r1-show-run.txt"
 
 routes = []
+interfaces = []
 
 with open(filnamn) as f:
     for rad in f:
         rad = rad.strip()
         if rad.startswith("ip route "):
             routes.append(rad)
+        elif rad.startswith("interface "):
+            interfaces.append(rad)
 
 print(f"Hittade {len(routes)} statiska rutter:")
 for rad in routes:
+    print(f" {rad}")
+
+print(f"Hittade {len(interfaces)} gränssnitt:")
+for rad in interfaces:
     print(f" {rad}")
