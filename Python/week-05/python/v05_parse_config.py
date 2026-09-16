00b6e7866d5d4f7ade61e2358ef05653aac10b63
Axw3ll3 <axel.hagelbergstrid@gmail.com>
2026-09-16 14:08:08 +0200

    Reworked code to show interfaces, and added pathlib path to be able to reach file from where you begin instead of needing to cd into the right directory. Added configs map and r1-show-run.txt file aswell

--- Python/week-05/python/v05_parse_config.py
+++ Python/week-05/python/v05_parse_config.py
@@ -1,13 +1,23 @@
-filnamn = "r1-show-run.txt"
+from pathlib import Path
+def main():
+    filnamn = Path("Python") / "week-05" / "python" / "configs" / "r1-show-run.txt"
+    routes = []
+    interfaces = []
+    
+    with open(filnamn) as f:
+        for rad in f:
+            rad = rad.strip()
+            if rad.startswith("ip route "):
+                routes.append(rad)
+            if rad.startswith("interface "):
+                interfaces.append(rad)
 
-routes = []
-
-with open(filnamn) as f:
-    for rad in f:
-        rad = rad.strip()
-        if rad.startswith("ip route "):
-            routes.append(rad)
-
-print(f"Hittade {len(routes)} statiska rutter:")
-for rad in routes:
-    print(f" {rad}")
+    print(f"Hittade {len(routes)} statiska rutter:")
+    for rad in routes:
+        print(f" {rad}")
+    print(f"Hittade {len(interfaces)} gränssnitt:")
+    for rad in interfaces:
+        print(f" {rad}")
+            
+if __name__ == "__main__":
+    main()
