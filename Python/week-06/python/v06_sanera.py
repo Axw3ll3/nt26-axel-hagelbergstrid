934463b86a5f1cf0ec94c47f171a6ce7a8605501
Axw3ll3 <axel.hagelbergstridh@gmail.com>
2026-09-24 10:13:45 +0200

    Made configs map and moved 41-rakonfig.txt and r1-show-run.txt

--- Python/week-06/python/v06_sanera.py
+++ Python/week-06/python/v06_sanera.py
@@ -3,7 +3,7 @@ from pathlib import Path
 def main():
     hemligt = ["secret", "password", "snmp-server community"]
     
-    filnamn = Path("Python") / "week-06" / "python" / "r1-rakonfig.txt"
+    filnamn = Path("Python") / "week-06" / "python" / "configs" / "r1-rakonfig.txt"
     ut_filnamn = filnamn.parent / "r1-show-run.txt"
     routes = []
     rensade = []
